40c59fdbe482efc0c8a5fa08a41450159a7bdeb4
FelipeCarneiroMachado <felipecmachado05@gmail.com>
2024-10-17 12:12:26 -0400

    a

--- Felipe_Carneiro_Machado_Maria_Rita_Gomides_Cruz_Hott.py
+++ Felipe_Carneiro_Machado_Maria_Rita_Gomides_Cruz_Hott.py
@@ -18,14 +18,19 @@ nx_graph = nx.from_numpy_array(graph)
 #fortemente conexo
 
 #verifica se qh maior que zero
-if np.any(graph == 0):
-    if not nx.is_connected(nx_graph):
-        print("Bixo SemVerTonha")
-        exit(0)
+# if np.any(graph == 0):
+if not nx.is_connected(nx_graph):
+    print("Bixo SemVerTonha")
+    exit(0)
 
 #Caso as condicoes do teorema de ferron-frobenius sejam satisfeitas
 #Basta pegar o maior autovalor e seu autovetor correspondente e encontrar a maior medida
 #de centralidade neste autovetor
 #AutoValores e auto vetores
-eigv = np.linalg.eig(graph.T)
+eigv = np.linalg.eig(graph)
+# print(graph)
+# print(eigv.eigenvalues)
+# print(eigv.eigenvectors)
+# print(eigv.eigenvalues.argmax())
+# print(eigv.eigenvectors[:, eigv.eigenvalues.argmax()])
 print(eigv.eigenvectors[:,eigv.eigenvalues.argmax()].argmax() + 1)
